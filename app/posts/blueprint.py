275bb89778136e341ab517f91c4892b54ccd1e97
AndreiLukashevich <rid7003482@gmail.com>
2018-01-28 21:17:52 +0300

    Added creation of posts

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,13 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
-from models import Post,Tag
+from models import Post, Tag
+from .forms import PostForm
+from app import db
+
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Something wrong')
+
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        posts = Post.query.all()
+
+    for post in posts:
+        if post.slug is None:
+            post.slug = 'None'
 
     return render_template('posts/index.html', posts=posts)
 
@@ -24,4 +55,3 @@ def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug == slug).first()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag=tag, posts=posts)
-
